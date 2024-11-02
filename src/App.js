e245d6086b4b6df5d34c6da1de2c86bf29f6072d
Chelaka <chelakalakshitha@gmail.com>
2024-11-02 21:30:39 +0530

    grid configure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardImages = [
+  { src: "/img/BunnyHat.png" },
+  { src: "/img/Coin.png" },
+  { src: "/img/MagicBall.png" },
+  { src: "/img/Scroll.png" },
+  { src: "/img/Shield.png" },
+  { src: "/img/Sword.png" },
+];
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+    console.log(cards, turns);
+  };
+
   return (
     <div className="App">
       <h1> Memory Game </h1>
-      <button>New game</button>
+      <button onClick={shuffleCards}>New game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card-front" />
+              <img className="back" src="/img/Cover.png" alt="card-back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
