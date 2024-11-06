1d6d9fc4b25158f0f2f5f0ab30ac34be685f2554
Chelaka <chelakalakshitha@gmail.com>
2024-11-06 16:49:02 +0530

    card matching implement

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/img/BunnyHat.png" },
-  { src: "/img/Coin.png" },
-  { src: "/img/MagicBall.png" },
-  { src: "/img/Scroll.png" },
-  { src: "/img/Shield.png" },
-  { src: "/img/Sword.png" },
+  { src: "/img/BunnyHat.png", matched: false },
+  { src: "/img/Coin.png", matched: false },
+  { src: "/img/MagicBall.png", matched: false },
+  { src: "/img/Scroll.png", matched: false },
+  { src: "/img/Shield.png", matched: false },
+  { src: "/img/Sword.png", matched: false },
 ];
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -21,16 +23,47 @@ function App() {
 
     setCards(shuffledCards);
     setTurns(0);
-    console.log(cards, turns);
   };
 
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        console.log("those cards match");
+        resetTurn();
+      } else {
+        console.log("those cards does not match");
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
   return (
     <div className="App">
       <h1> Memory Game </h1>
       <button onClick={shuffleCards}>New game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
