be7d87be14984a63bd9f29507205e222430f1c83
Chelaka <chelakalakshitha@gmail.com>
2024-11-11 18:55:27 +0530

    Main Page added

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MainPage() {
+  return (
+    <div>
+      <>
+        <button>New Game</button>
+        <buton>How To Play</buton>
+        <button>Leaderboards</button>
+      </>
+    </div>
+  );
+}
+
+export default MainPage;
