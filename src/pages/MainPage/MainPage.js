9a2b05300346f42631e6e91b5003cf83167c918d
ChelakaLiyanage <chelakalakshitha@gmail.com>
2024-11-14 19:47:27 +0530

    game difficulty choose part in main page

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { routeHelper } from "../../helpers/routeHelper";
@@ -6,11 +6,27 @@ import { routeHelper } from "../../helpers/routeHelper";
 const MainPage = () => {
   const navigate = useNavigate();
 
+  const [difficulty, setDifficulty] = useState("normal");
+
+  const startGame = () => {
+    navigate(routeHelper.GAMEPAGE.PATH, { state: { difficulty } });
+  };
+
   return (
     <div>
-      <button onClick={() => navigate(routeHelper.GAMEPAGE.PATH)}>
-        New Game
-      </button>
+      <h1>Memory Game</h1>
+      <div>
+        <label>Select difficulty</label>
+        <select
+          value={difficulty}
+          onChange={(e) => setDifficulty(e.target.value)}
+        >
+          <option value="easy">Easy</option>
+          <option value="normal">Normal</option>
+          <option value="hard">Hard</option>
+        </select>
+      </div>
+      <button onClick={startGame}>New Game</button>
       <button>How To Play</button>
       <button>Leaderboards</button>
     </div>
