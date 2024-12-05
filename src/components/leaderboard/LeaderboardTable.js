ea493d058f071d807ef007cce7ae02b3a3ee7845
Chelaka <chelakalakshitha@gmail.com>
2024-12-05 22:37:27 +0530

    table pagination

--- src/components/leaderboard/LeaderboardTable.js
+++ src/components/leaderboard/LeaderboardTable.js
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TablePagination from "@mui/material/TablePagination";
 
 import { useLeaderboardContext } from "../../providers/LeaderboardProvider";
 
 const LeaderboardTable = () => {
   const { leaderboardData } = useLeaderboardContext();
 
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const displayedData = leaderboardData.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
+
   return (
-    <div style={{ width: "100%", justifyContent: "center" }}>
+    <div style={{ width: "90%", justifyContent: "center" }}>
       <TableContainer>
         <Table>
           <TableHead>
@@ -22,22 +41,36 @@ const LeaderboardTable = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {leaderboardData.length > 0 ? (
-              leaderboardData.map((leader, index) => (
+            {displayedData.length > 0 ? (
+              displayedData.map((leader, index) => (
                 <TableRow key={leader.gameId}>
-                  <TableCell align="center">{index + 1}</TableCell>
+                  <TableCell align="center">
+                    {page * rowsPerPage + index + 1}
+                  </TableCell>
                   <TableCell align="center">{leader.userName}</TableCell>
                   <TableCell align="center">{leader.score}</TableCell>
                 </TableRow>
               ))
             ) : (
               <TableRow>
-                <TableCell align="center"> no data </TableCell>
+                <TableCell align="center" colSpan={3}>
+                  {" "}
+                  no data{" "}
+                </TableCell>
               </TableRow>
             )}
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+        component="div"
+        count={leaderboardData.length}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        rowsPerPageOptions={[10]}
+      />
     </div>
   );
 };
