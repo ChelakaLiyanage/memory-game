4d875a8730cf10ff22a2bb6c78ee3ef9066fe6cf
Chelaka <chelakalakshitha@gmail.com>
2024-12-05 22:40:31 +0530

    firebase message getter

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import { firebaseHelper } from "../helpers/firebaseHelper";
+
 export const formatTime = (timeInSeconds) => {
   const minutes = Math.floor(timeInSeconds / 60);
   const seconds = timeInSeconds % 60;
@@ -5,4 +7,21 @@ export const formatTime = (timeInSeconds) => {
     2,
     "0"
   )}`;
+};
+
+export const getFirebaseErrorMessage = (errorCode) => {
+  const { ERROR_CODES } = firebaseHelper;
+
+  console.log(errorCode);
+
+  switch (errorCode) {
+    case ERROR_CODES.INVALID_CREDENTIALS.CODE:
+      return ERROR_CODES.INVALID_CREDENTIALS.MESSAGE;
+    case ERROR_CODES.TOO_MANY_REQUESTS.CODE:
+      return ERROR_CODES.TOO_MANY_REQUESTS.MESSAGE;
+    case ERROR_CODES.EMAIL_EXISTS.CODE:
+      return ERROR_CODES.EMAIL_EXISTS.MESSAGE;
+    default:
+      return ERROR_CODES.DEFAULT_MESSAGE;
+  }
 };
